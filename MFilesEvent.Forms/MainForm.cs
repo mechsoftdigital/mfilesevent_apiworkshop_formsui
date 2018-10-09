e9c234846ecbf5f1e348b681d6d50c0a44ddb3e0
Gökay Kıvırcıoğlu <riza.gokay@gmail.com>
2018-10-09 15:25:01 +0300

    Filled loaddocuments function

--- MFilesEvent.Forms/MainForm.cs
+++ MFilesEvent.Forms/MainForm.cs
@@ -21,6 +21,8 @@ namespace MFilesEvent.Forms
         {
             InitializeComponent();
             _loggedInVault = loggedInVault;
+            listView1.View = System.Windows.Forms.View.Details;
+   
         }
 
         private void listItemMouseDoubleClick(object sender, MouseEventArgs e)
@@ -31,12 +33,56 @@ namespace MFilesEvent.Forms
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            LoadDocuments();
         }
 
         private void LoadDocuments()
         {
+            listView1.Items.Clear();
 
+            int count = 0;
+
+            //Build Search Conditions
+            var scs = new SearchConditions();
+            var sc = new SearchCondition();
+
+            //Object Type Condition (Document)
+            sc.ConditionType = MFConditionType.MFConditionTypeEqual;
+            sc.Expression.DataStatusValueType = MFStatusType.MFStatusTypeObjectTypeID;
+            sc.TypedValue.SetValue(MFDataType.MFDatatypeLookup, 0);
+            scs.Add(-1, sc);
+
+            //Deleted Condition (Not Deleted)
+            sc.ConditionType = MFConditionType.MFConditionTypeEqual;
+            sc.Expression.DataStatusValueType = MFStatusType.MFStatusTypeDeleted;
+            sc.TypedValue.SetValue(MFDataType.MFDatatypeBoolean, false);
+            scs.Add(-1, sc);
+
+            //Perform Search & Enumerate
+            _loggedInVault
+                .ObjectSearchOperations
+                .SearchForObjectsByConditions(scs, MFSearchFlags.MFSearchFlagNone, false)
+                .Cast<ObjectVersion>()
+                .OrderByDescending(x => x.ObjVer.ID)
+                .ToList().ForEach(x =>
+                {
+                    //If it has files :( 
+                    if (x.Files.Count > 0 && x.SingleFile)
+                    {
+                        var firstFile = x.Files[1];
+
+                        //Poulate listview items
+                        var lvItem = new ListViewItem(new string[] { $"{firstFile.Title}.{firstFile.Extension}", "Document" });
+                        lvItem.Tag = new SimpleInfo { Id = x.ObjVer.ID, Type = x.ObjVer.Type };
+
+                        listView1.Items.Add(lvItem);
+                        count++;
+                    }
+
+                });
+
+            //Set item count to label.
+            lblCount.Text = count.ToString();
 
         }
 
